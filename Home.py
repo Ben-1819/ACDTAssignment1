76d497fd36691604b27b2faff51bafdda9cf1718
Ben-1819 <ben.brown20332@gmail.com>
2025-12-10 15:59:34 +0000

    feat: Added some business use cases to Home page

--- Home.py
+++ Home.py
@@ -9,14 +9,14 @@ st.write("ACDT Assignment 1")
 st.markdown(
     """
     # CryptoPrice
-    Uses a cryptocurrency price API
+    Uses a cryptocurrency price API. This is useful to a business that may need access to fast, accurate data such as companies that monitor cryptocurrency markets. Real time pricing allows companies to make better informed decisions.
     # IPCall
-    Gets the users location and displays it on a map
+    Gets the users location and displays it on a map. This is useful in a business use case as it allows businesses to automatically determine a user's geographical location and personalise services to them or monitor where their users are from
     # NewsCall
-    Allows the user to choose 1 or 2 keywords and then get 5 news articles containing those keywords
+    Allows the user to choose 1 or 2 keywords and then get 5 news articles containing those keywords. Can be used in a business use case by allowing organisations to monitor relevant news to their industry such as information on new technologies or events that may impact them
     # WeatherByLocation
     Combined OpenWeatherMap API and ip-api to get the weather in the users current location
     # WeatherCall
-    Lets the user enter a city that they want to get the weather for
+    Lets the user enter a city that they want to get the weather for - Provides up to date weather information which can be used for a variety of different things such as field work planning or delivery route monitoring 
     """
     )
